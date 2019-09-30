9321878091c5d1243d3ae0382a20d63c76f1e765
pradeepswain <pswain@activehealth.net>
2019-10-01 02:05:30 +0530

    Adding New iOS code

---
+++ BankNoteApp/Controller/HomeController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeController.swift
+//  BankNoteApp
+//
+//  Created by Pradeep on 30/09/19.
+//  Copyright © 2019 Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var delegate: HomeControllerDelegate?
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        configureNavigationBar()
+    }
+    
+    // MARK: - Handlers
+    
+    @objc func handleMenuToggle() {
+        delegate?.handleMenuToggle(forMenuOption: nil)
+    }
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .darkGray
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationItem.title = "My Collection"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_white_3x").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleMenuToggle))
+    }
+}
---
+++ BankNoteApp/Utils/Protocols.swift
@@ -0,0 +1,11 @@
+//
+//  Protocols.swift
+//  BankNoteApp
+//
+//  Created by Pradeep on 30/09/19.
+//  Copyright © 2019 Pradeep. All rights reserved.
+//
+
+protocol HomeControllerDelegate {
+    func handleMenuToggle(forMenuOption menuOption: MenuOption?)
+}
